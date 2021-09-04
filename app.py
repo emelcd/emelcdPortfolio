64236547bc4062beb26d275ef37c8a0d10a74571
emelcd <mick.altura@gmail.com>
2021-09-04 03:17:47 +0200

    Exploring

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from os import environ
+from uuid import uuid4
+import json
 
+api_key = environ.get('API_KEY')
 
 app = Flask(__name__)
 
@@ -7,6 +11,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template(template_name_or_list='index.html')
+
+@app.route('/msg')
+def about():
+    with open('msg.json', 'r') as f:
+        msg = json.load(f)
+    
+    msg[str(uuid4())] = {
+        'msg': 'Hello World!',
+        'name': api_key
+    }
+
+    with open('msg.json', 'w') as f:
+        json.dump(msg, f)
+
+    return jsonify(msg)
 
 
 if __name__ == '__main__':
